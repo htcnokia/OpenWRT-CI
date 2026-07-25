6f09e8f46a0faf7e26bad9d1ba80b4281dac00c3
htcnokia <95518596+htcnokia@users.noreply.github.com>
2026-07-25 16:37:32 +0800

    Uncomment cloning for luci-app-oaf source

--- Scripts/PRIVATE.sh
+++ Scripts/PRIVATE.sh
@@ -50,7 +50,7 @@ git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-lucky.git packa
 #echo "[克隆] 正在克隆 luci-app-netspeedtest 源码..."
 #git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest.git package/netspeedtest
 
-#echo "[克隆] 正在克隆 luci-app-oaf 源码..."
+echo "[克隆] 正在克隆 luci-app-oaf 源码..."
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 echo "=========================================="
