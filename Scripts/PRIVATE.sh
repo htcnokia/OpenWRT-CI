e3b1c2a9de5affb3d52355c58389133b2e69921d
htcnokia <95518596+htcnokia@users.noreply.github.com>
2026-07-31 22:30:07 +0800

    Comment out cleanup and clone commands in PRIVATE.sh
    Comment out cleanup and clone commands for luci-app-timecontrol and luci-app-netspeedtest.

--- Scripts/PRIVATE.sh
+++ Scripts/PRIVATE.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-echo "[清理] 正在清理 luci-app-timecontrol 源码..."
-rm -rf package/luci-app-timecontrol
-rm -rf luci-app-timecontrol
-rm -rf package/feeds/luci/luci-app-timecontrol
-rm -rf package/feeds/packages/luci-app-timecontrol
+#echo "[清理] 正在清理 luci-app-timecontrol 源码..."
+#rm -rf package/luci-app-timecontrol
+#rm -rf luci-app-timecontrol
+#rm -rf package/feeds/luci/luci-app-timecontrol
+#rm -rf package/feeds/packages/luci-app-timecontrol
 
 #echo "[克隆] 正在克隆luci-app-timecontrol 源码..."
 #git clone -b js --depth=1 https://github.com/gaobin89/luci-app-timecontrol.git package/luci-app-timecontrol
 
-echo "[克隆] 正在克隆 luci-app-lucky 源码..."
-git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
-
-#echo "[克隆] 正在克隆 luci-app-netspeedtest 源码..."
-#git clone -b main --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest.git package/netspeedtest
 
