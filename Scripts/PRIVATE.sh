999783429dc06f537041e895108fe73b5195476e
htcnokia <95518596+htcnokia@users.noreply.github.com>
2026-07-31 22:49:58 +0800

    Update PRIVATE.sh

--- Scripts/PRIVATE.sh
+++ Scripts/PRIVATE.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#echo "[清理] 正在清理 luci-app-timecontrol 源码..."
-#rm -rf package/luci-app-timecontrol
-#rm -rf luci-app-timecontrol
-#rm -rf package/feeds/luci/luci-app-timecontrol
-#rm -rf package/feeds/packages/luci-app-timecontrol
+echo "[清理] 正在清理 luci-app-timecontrol 源码..."
+rm -rf package/luci-app-timecontrol
+rm -rf luci-app-timecontrol
+rm -rf package/feeds/luci/luci-app-timecontrol
+rm -rf package/feeds/packages/luci-app-timecontrol
 
-#echo "[克隆] 正在克隆luci-app-timecontrol 源码..."
-#git clone -b js --depth=1 https://github.com/gaobin89/luci-app-timecontrol.git package/luci-app-timecontrol
+echo "[克隆] 正在克隆luci-app-timecontrol 源码..."
+git clone -b js --depth=1 https://github.com/gaobin89/luci-app-timecontrol.git package/luci-app-timecontrol
 
 
