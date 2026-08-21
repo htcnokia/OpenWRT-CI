07b867089a314ce0a1197421efc15abaad8a4d55
htcnokia <95518596+htcnokia@users.noreply.github.com>
2026-08-21 21:46:15 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -125,15 +125,3 @@ UPDATE_VERSION() {
 #UPDATE_VERSION "软件包名" "测试版，true，可选，默认为否"
 #UPDATE_VERSION "sing-box"
 
-
-
-UPDATE_PACKAGE "luci-app-gecoosac" "laipeng668/luci-app-gecoosac" "main" "" "gecoosac luci-app-gecoosac"
-
-mkdir -p files/etc
-touch files/etc/sysupgrade.conf
-echo "/etc/homeproxy/private_srs" >> files/etc/sysupgrade.conf
-chmod 644 files/etc/sysupgrade.conf
-#引入私有扩展脚本
-if [ -f "$GITHUB_WORKSPACE/Scripts/PRIVATE.sh" ]; then
-	source "$GITHUB_WORKSPACE/Scripts/PRIVATE.sh"
-fi
